97eb2112a863f65d804423a9784d21a57f5ba0d4
Skiy <skiy@outlook.com>
2018-10-03 17:15:01 +0100

    returned to pre php, it isn't working for me atm.
    used a footer tag for the footer

--- footer.php
+++
@@ -1,9 +0,0 @@
-  <p align="center">
-  Jack Lloyd-Walters ©
-  <script type="text/javascript">
-    var today = new Date()
-    var year = today.getFullYear()
-    document.write(year)
-    </script>
-  </p>
-</html>
--- template.php
+++
@@ -1,7 +0,0 @@
-<?php include("header.php"); ?>
-<body>
-
-	content goes here
-
-</body>
-<?php include('footer.php'); ?>
