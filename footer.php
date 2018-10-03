14dabcfdcbe03f512b05dfa45aa876ef2631e547
Skiy <skiy@outlook.com>
2018-10-03 16:35:07 +0100

    experimenting with php headers

---
+++ footer.php
@@ -0,0 +1,10 @@
+<html>
+  <p align="center">
+  Jack Lloyd-Walters ©
+  <script type="text/javascript">
+    var today = new Date()
+    var year = today.getFullYear()
+    document.write(year)
+    </script>
+  </p>
+</html>
